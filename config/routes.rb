591f5af56dac0c18249bfb5db23178bff24dfc63
romitbugna <rbugna@bestpeers.com>
2021-09-30 22:42:29 +0530

    adding resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "homes#index"
-  devise_for :users, :teams, :customers, :schedule, :booking
+  resources :bookings
+  resources :buses
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
