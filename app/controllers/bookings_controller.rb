3c346bef48e94068620f2f81c6393c86fdc7cfc6
romitbugna <rbugna@bestpeers.com>
2021-10-01 23:04:01 +0530

    modification in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,65 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @user = User.find(session[:user_id])
+    respond_to do |format|
+        format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
+        format.json { render :show, status: :created, location: @booking }
+    
+      else
+        format.html { render :new }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+  def update
+    respond_to do |format|
+      if @booking.update(booking_params)
+        format.html { redirect_to @booking, notice: 'Booking was successfully updated.' }
+        format.json { render :show, status: :ok, location: @booking }
+      else
+        format.html { render :edit }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    respond_to do |format|
+      format.html { redirect_to bookings_url, notice: 'Booking was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_booking
+      @booking = Booking.find(params[:id])
+    end
+       
+
+    def booking_params
+      params.require(:booking).permit(:total_seat, :seat_type, :bus_name, :bus_no, :bus_destination, :user_id)
+      end
+
 end
--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,2 +1,62 @@
 class BusesController < ApplicationController
+    def index
+        @buses = Bus.all
+    end
+
+    def show
+    end
+
+    def new
+        @bus = Bus.new
+    end
+
+    def edit
+    end
+
+    def create
+        @bus = Bus.new(bus_params)
+    
+        respond_to do |format|
+          if @bus.save
+            format.html { redirect_to @bus, notice: 'Bus was successfully created.' }
+            format.json { render :show, status: :created, location: @bus }
+          else
+            format.html { render :new }
+            format.json { render json: @bus.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      def update
+        respond_to do |format|
+          if @bus.update(bus_params)
+            format.html { redirect_to @bus, notice: 'Bus was successfully updated.' }
+            format.json { render :show, status: :ok, location: @bus }
+          else
+            format.html { render :edit }
+            format.json { render json: @bus.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      def destroy
+        @bus.destroy
+        respond_to do |format|
+          format.html { redirect_to buses_url, notice: 'Bus was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
+
+      private
+       
+        def set_bus
+          @bus = Bus.find(params[:id])
+        end
+
+        def bus_params
+          params.require(:bus).permit(:name)
+        end
+    end
 end
+
+
