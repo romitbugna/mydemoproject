84bca8999299a86fc1623bad13d4f10644e513f4
romitbugna <rbugna@bestpeers.com>
2021-10-05 19:58:41 +0530

    mofications in models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,4 @@
 class Booking < ApplicationRecord
-    belongs_to :user
+    # belongs_to :user
+    SEAT_TYPE_LIST = ["Lower Class", "Middle Class", "Higher Class"]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :booking
   has_many :schedule
+  has_many :buses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
