0a3b3e22dfeccae9bd99f60c8d3e866f1eacc47f
romitbugna <rbugna@bestpeers.com>
2021-10-07 23:15:41 +0530

    modified in models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-    # belongs_to :user
     SEAT_TYPE_LIST = ["Lower Class", "Middle Class", "Higher Class"]
+    COST_OF_TICKET = ["300", "500", "1000"]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :booking
+  # has_many :booking
   has_many :schedule
   has_many :buses
   # Include default devise modules. Others available are:
