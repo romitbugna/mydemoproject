db49691162fcb8eef41564dfc941e57b8e5eab64
romitbugna <rbugna@bestpeers.com>
2021-10-08 21:32:03 +0530

    modified in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # has_many :booking
   has_many :schedule
-  has_many :buses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
