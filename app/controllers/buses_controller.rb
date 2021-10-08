217a6970f9a41a68b25b936cee38f2d18d143870
romitbugna <rbugna@bestpeers.com>
2021-10-08 21:25:47 +0530

    modifications in buses controller

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,63 +1,63 @@
 class BusesController < ApplicationController
   before_action :set_bus, only: [:show, :edit, :update, :destroy]
-    def index
-        @buses = Bus.all
-    end
+  def index
+    @buses = Bus.all
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
-        @bus = Bus.new
-    end
+  def new
+    @bus = Bus.new
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def create
-      @bus = Bus.new(bus_params)
+  def create
+    @bus = Bus.new(bus_params)
     
-      respond_to do |format|
-        if @bus.save
-          format.html { redirect_to @bus, notice: 'Bus was successfully created.' }
-          format.json { render :show, status: :created, location: @bus }
-        else
-          format.html { render :new }
-          format.json { render json: @bus.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+    if @bus.save
+      format.html { redirect_to @bus, notice: 'Bus was successfully created.' }
+      format.json { render :show, status: :created, location: @bus }
+    else
+      format.html { render :new }
+      format.json { render json: @bus.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-      def update
-        respond_to do |format|
-          if @bus.update(bus_params)
-            format.html { redirect_to @bus, notice: 'Bus was successfully updated.' }
-            format.json { render :show, status: :ok, location: @bus }
-          else
-            format.html { render :edit }
-            format.json { render json: @bus.errors, status: :unprocessable_entity }
-          end
-        end
+  def update
+    respond_to do |format|
+    if @bus.update(bus_params)
+      format.html { redirect_to @bus, notice: 'Bus was successfully updated.' }
+      format.json { render :show, status: :ok, location: @bus }
+    else
+      format.html { render :edit }
+      format.json { render json: @bus.errors, status: :unprocessable_entity }
       end
+    end
+  end
 
-      def destroy
-        @bus.destroy
-        respond_to do |format|
-          format.html { redirect_to buses_url, notice: 'Bus was successfully destroyed.' }
-          format.json { head :no_content }
-        end
-      end
+  def destroy
+    @bus.destroy
+    respond_to do |format|
+      format.html { redirect_to buses_url, notice: 'Bus was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
-      private
+  private
        
-        def set_bus
-          @bus = Bus.find(params[:id])
-        end
+  def set_bus
+    @bus = Bus.find(params[:id])
+  end
 
-        def bus_params
-          params.require(:bus).permit(:name)
-        end
-    end
+  def bus_params
+    params.require(:bus).permit(:bus_name,:bus_no, :seat_type,:from, :to, :date, :time)
+  end
+end
   
 
 
