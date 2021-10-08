3c6b15039ec9fdba2f528c7c599bcb5b160434b7
romitbugna <rbugna@bestpeers.com>
2021-10-08 21:28:54 +0530

    modifications in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :bookings
   resources :buses
   devise_for :users
+  get 'homes-about', to: "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
